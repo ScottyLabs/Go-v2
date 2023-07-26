3ed9175fb9c5968fb5657a0a8191b3fda66400fb
Gabriel Hall <gabriel.hall@cox.net>
2023-07-26 00:27:00 -0700

    add theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,17 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { trpc } from "../utils/trpc";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Toaster />
-      <CreateRouteDialog />
-      <EditRouteDialog />
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <Toaster />
+        <CreateRouteDialog />
+        <EditRouteDialog />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
