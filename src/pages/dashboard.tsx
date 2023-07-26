e674b02fffee925c7ccc507c2b10363f09814c53
Gabriel Hall <gabriel.hall@cox.net>
2023-07-26 01:02:45 -0700

    fix overflow

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -11,15 +11,19 @@ export default function Page() {
   if (!routes.isSuccess) return <div>Loading...</div>;
 
   return (
-    <div className="container space-y-8 p-8">
-      <div className="flex items-center justify-between">
-        <h1 className="text-3xl font-semibold">Routes</h1>
-        <Button variant="outline" onClick={() => setDialog("createRoute")}>
-          Add
-        </Button>
+    <div className="min-h-screen overflow-hidden">
+      <div className="space-y-8 p-8">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-semibold">Routes</h1>
+          <Button variant="outline" onClick={() => setDialog("createRoute")}>
+            Add
+          </Button>
+        </div>
+        <hr />
+        <div className="relative">
+          <RoutesTable data={routes.data} />
+        </div>
       </div>
-      <hr />
-      <RoutesTable data={routes.data} />
     </div>
   );
 }
